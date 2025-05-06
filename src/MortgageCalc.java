32f8bd6fbf41e120e3d92e96e0936c0852851146
German <ryndagerman@gmail.com>
2025-05-06 22:42:00 +0300

    Last commit

--- src/MortgageCalc.java
+++ src/MortgageCalc.java
@@ -1,4 +1,53 @@
-package PACKAGE_NAME;
-
 public class MortgageCalc {
+
+    private final static byte MONTHS_IN_YEAR = 12;
+    private final static byte PERCENT = 100;
+
+    private static int principal;
+    private static float annualInterest;
+    private static byte years;
+
+    public MortgageCalc(int principal, byte years, float annualInterest) {
+        this.principal = principal;
+        this.years = years;
+        this.annualInterest = annualInterest;
+    }
+
+    public static double calculateBalance(short numberOfPaymentsMade) {
+        float monthlyInterest = getMonthlyInterest();
+        float numberOfPayments = getNumberOfPayments();
+
+        double balance = principal
+                * (Math.pow(1 + monthlyInterest, numberOfPayments) - Math.pow(1 + monthlyInterest, numberOfPaymentsMade))
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+
+        return balance;
+    }
+
+    public double calculateMortgage() {
+
+        float monthlyInterest = getMonthlyInterest();
+        float numberOfPayments = getNumberOfPayments();
+
+        double mortgage = principal
+                * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+
+        return mortgage;
+    }
+
+    public double[] getRemainingBalances() {
+        var balances = new double[getNumberOfPayments()];
+        for (short month = 1; month <= balances.length; month++)
+            balances[month - 1] = calculateBalance(month);
+        return balances;
+    }
+
+    private static float getMonthlyInterest() {
+        return annualInterest / PERCENT / MONTHS_IN_YEAR;
+    }
+
+    private static int getNumberOfPayments() {
+        return years * MONTHS_IN_YEAR;
+    }
 }
--- src/MortgageRep.java
+++ src/MortgageRep.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
+import java.text.NumberFormat;
 public class MortgageRep {
+    private MortgageCalc calculator;
+    private NumberFormat currency = NumberFormat.getCurrencyInstance();;
+
+    public MortgageRep(MortgageCalc calculator) {
+        this.calculator = calculator;
+    }
+
+    public void printPaymentSchedule() {
+        System.out.println();
+        System.out.println("PAYMENT SCHEDULE");
+        System.out.println("----------------");
+        for (double balance : calculator.getRemainingBalances())
+            System.out.println(currency.format(balance));
+    }
+    public void printMortgage() {
+        double mortgage = calculator.calculateMortgage();
+        String mortgageFormatted = currency.format(mortgage);
+        System.out.println();
+        System.out.println("MORTGAGE");
+        System.out.println("--------");
+        System.out.println("Monthly Payments: " + mortgageFormatted);
+    }
 }
